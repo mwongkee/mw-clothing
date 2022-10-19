6910248c0b870f6bf20ed49f2fbc962030363acb
Matthew Wongkee <matt.wongkee@gmail.com>
2022-10-19 13:44:33 -0400

    sign up form layout

--- mw-clothing/src/routes/sign-in/sign-in.component.jsx
+++ mw-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 import { 
   auth,
@@ -18,6 +19,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   )
 }
